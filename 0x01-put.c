3ed6d8a4a75287b8ebf14723deeff8effb29669c
Massahgenesis <massyndjao@gmail.com>
2023-03-28 03:39:20 -0700

    Program with puts functions

---
+++ 0x01-put.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <unistd.h>
+/**
+ *_putc - Function that print caracter
+ *@c: A caracter
+ *Return: void
+ */
+int _putc(char c)
+{
+	int n = (write(1, &c, 1));
+
+	return ((n < 0) ? 0 : n);
+}
+/**
+ *_puts - Function that print string
+ *@str: String to print
+ *Return: Number of caracter printed
+ */
+int _puts(char *str)
+{
+	int i = 0;
+	int n = 0;
+
+	if (str == 0)
+		return (0);
+	while (str[i] != '\0')
+	{
+		n += _putc(str[i]);
+		i++;
+	}
+	return (n);
+}
