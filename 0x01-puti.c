a5dac596049b3e05fe558e5f54aaaa0246e11ae1
Nkentseu <nkentseu@gmail.com>
2023-03-28 11:18:07 +0000

    add a puti to print ingerer

---
+++ 0x01-puti.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <limits.h>
+
+/**
+ * _puti - print the integer number
+ * @number: the number was printing
+ * Return: number of digit print
+ */
+int _puti(int number)
+{
+	if (numbre < 0)
+	{
+		int n = _putc('-');
+
+		if (n == INT_MIN)
+		{
+			n += _puti(-(n / 10));
+			n += _putc('0' + (-(n % 10)));
+		}
+		else
+			n += _puti(-n);
+		return (n);
+	}
+	else
+	{
+		int n = 0;
+
+		if (number <= 9)
+			return (_putc('0' + n));
+		n += _puti(n / 10);
+		n += _putc(n % 10);
+		return (n);
+	}
+}
--- main.h
+++ main.h
@@ -7,6 +7,9 @@
 int _putc(char c);
 int _puts(char *str);
 
+/* a creer dans le fichier 0x01-puti.c pour lire ecrire des entiers*/
+int _puti(int number);
+
 /* a creer dans le fichier 0x01-printf.c*/
 int _printf(char *format, ...);
 
