5f698f80136da9520149ff4805f76ba507a6de93
Nkentseu <117810623+Nkentseu@users.noreply.github.com>
2023-03-28 10:44:30 +0100

    Create main.h

---
+++ main.h
@@ -0,0 +1,13 @@
+#ifndef __MAIN_H__
+#define __MAIN_H__
+
+#include <stdarg.h>
+
+/* a creer dans le fichier 0x01-put.c*/
+int _putc(char c);
+int _puts(char *str);
+
+/* a creer dans le fichier 0x01-printf.c*/
+int _printf(char *format, ...)
+
+#endif
